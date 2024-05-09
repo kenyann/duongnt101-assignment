e9441f538b4e5a1b060fdc909a3dbd8d2f6672c6
Duong Nguy <duongnt.cnthongtin@gmail.com>
2024-05-09 22:20:19 +0700

    connect to Azure Blob Storage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # duongnt101-assignment
+https://medium.com/@datafairy/airflow-and-azure-storage-accounts-4db1c475b85d
--- dags/etl.py
+++ dags/etl.py
@@ -4,8 +4,8 @@ from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.sensors.python import PythonSensor
-
-
+from airflow.providers.microsoft.azure.transfers.local_to_adls import LocalFilesystemToADLSOperator
+from airflow.providers.microsoft.azure.transfers.local_to_wasb import LocalFilesystemToWasbOperator
 # nopep8
 import sys
 sys.path.insert(0, "./")
@@ -14,49 +14,51 @@ from src.crawler import Crawler  # nopep8
 
 default_args = {
     "owner": "Duong_Nguy",
-    "retries": 5,
+    "retries": 1,
     "retry_delay": timedelta(minutes=5)
 }
 
 
-def _extract():
+@task(task_id="extract")
+def extract():
     import pandas as pd
     crawl = Crawler('https://www.dienmayxanh.com/may-lanh#c=2002&o=13&pi=7')
-    result = crawl.extract()
-    result.to_csv("test.csv", index=False)
+    return crawl.extract()  # have to be json
 
 
-def _transform():
-    pass
-
-
-def _load():
-    pass
+def transform(df):
+    df.to_csv("./duongnt101-assignment/may-lanh.csv", index=False)
 
 
 @dag(dag_id="etl", start_date=datetime(2024, 5, 9), default_args=default_args, catchup=False)
 def pipeline():
 
-    extract = PythonOperator(
+    extract_task = PythonOperator(
         task_id="crawl_data",
-        python_callable=_extract
+        python_callable=extract
     )
 
-    transform = PythonOperator(
+    transform_task = PythonOperator(
         task_id="transform_data",
-        python_callable=_transform
+        op_kwargs={"df": "{{ti.xcom_pull('extract')}}"},
+        python_callable=transform
     )
 
-    load = PythonOperator(
-        task_id="load_data",
-        python_callable=_load
-    )
+    # load = LocalFilesystemToADLSOperator(
+    #     task_id="upload_task",
+    #     local_path="test.csv",
+    #     remote_path="https://duongnt101.blob.core.windows.net/dmx/test.csv",
+    #     azure_data_lake_conn_id="adls_v2"
+    # )
 
-    # check_data = check_data_available(PATH)
-    # check_data >> [check_data_success, check_data_fail]
-    # check_data_success >> run_process >> [
-    #     run_process_fail, run_process_success]
-    extract >> transform >> load
+    load_task = LocalFilesystemToWasbOperator(
+        task_id="upload_file",
+        file_path="./duongnt101-assignment/may-lanh.csv",
+        container_name="dmx",
+        blob_name="may-lanh.csv",
+        wasb_conn_id="abs"
+    )
+    extract_task >> transform_task >> load_task
 
 
 pipeline()
